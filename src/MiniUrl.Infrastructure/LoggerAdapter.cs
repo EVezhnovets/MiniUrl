ab28256cc02e07f2794c68cc6bc9300aae303137
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-08-02 10:36:13 +0300

    Added MiniUrl.Infrastructure project to solution with Serilog packages, LoggerAdapter.cs

---
+++ src/MiniUrl.Infrastructure/LoggerAdapter.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MiniUrl.ApplicationCore.Interfaces;
+using Serilog;
+
+namespace MiniUrl.Infrastructure
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILoggerFactory loggerFactory, IConfiguration configuration)
+        {
+            var logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+            _logger = loggerFactory.AddSerilog(logger).CreateLogger<T>();
+        }
+
+        public void LogError(Exception exception, string message, params object[] args)
+        {
+            _logger.LogError(exception, message, args);
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+    }
+}
