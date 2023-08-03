a1a9869ca4ee91fc58b02622d1cd511945d59608
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-08-03 11:36:23 +0300

    Added connection string to appsettings.dev.json, EF context class and configure it by Dependencies class, in which add other services

---
+++ src/MiniUrl.Infrastructure/Dependencies.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using MiniUrl.ApplicationCore.Interfaces;
+
+namespace MiniUrl.Infrastructure
+{
+    public static class Dependencies
+    {
+        public static void ConfigureServices(IConfiguration configuration, IServiceCollection services, ILoggingBuilder loggingBuilder)
+        {
+            services.AddDbContext<MiniUrlContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("miniUrlConnection")));
+
+            services.AddSingleton(typeof(IAppLogger<>), typeof(LoggerAdapter<>));
+        }
+    }
+}
