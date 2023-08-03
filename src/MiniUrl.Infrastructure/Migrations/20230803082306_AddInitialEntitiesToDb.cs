1556bf8c756960f65c1d062a0ff0f78d1778af49
eugene.vezhnavets@gmail.com <eugene.vezhnavets@gmail.com>
2023-08-03 11:37:33 +0300

    Added initial migration

---
+++ src/MiniUrl.Infrastructure/Migrations/20230803082306_AddInitialEntitiesToDb.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniUrl.Infrastructure.Migrations
+{
+    public partial class AddInitialEntitiesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MiniUrlItems",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Domain = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Alias = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tags = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MiniUrlItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MiniUrlOfUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MiniUrl = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MiniUrlOfUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MiniUrlItems");
+
+            migrationBuilder.DropTable(
+                name: "MiniUrlOfUsers");
+        }
+    }
+}
